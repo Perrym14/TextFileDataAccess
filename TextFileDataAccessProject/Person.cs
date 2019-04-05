8d9512be5612b7c65e0ae2d5afa970ba6bf01bdf
Malik Perry <Malikap14@gmail.com>
2019-04-05 13:10:47 -0700

    Created Person class. Parsed txt document info to Person class.

---
+++ TextFileDataAccessProject/Person.cs
@@ -0,0 +1,19 @@
+﻿namespace TextFileDataAccessProject
+{
+    public class Person
+    {
+        private string FirstName { get; set; }
+        private string LastName { get; set; }
+        private string Url { get; set; }
+        public string Format { get; set; }
+
+        public Person(string firstName, string lastName, string url)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Url = url;
+
+            Format = $"{FirstName} {LastName}: {Url}";
+        }
+    }
+}
--- TextFileDataAccessProject/Program.cs
+++ TextFileDataAccessProject/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TextFileDataAccessProject
 {
@@ -10,6 +9,28 @@ namespace TextFileDataAccessProject
     {
         static void Main(string[] args)
         {
+            string filePath = @"C:\Practice\Example.txt";
+
+            List<Person> people = new List<Person>();
+
+            List<string> lines = File.ReadAllLines(filePath).ToList();
+
+            foreach (var line in lines)
+            {
+                string[] entries = line.Split(',');
+
+                Person newPerson = new Person(entries[0], entries[1], entries[2]);
+
+                people.Add(newPerson);
+            }
+
+            //Check to make sure foreach works as intended.
+            foreach (var person in people)
+            {
+                Console.WriteLine($"{person.Format}");
+            }
+
+            Console.ReadLine();
         }
     }
 }
